887ebd04a5fff4bcade0abf7f889b75a8e2c21e2
William Criqui <wocriqui@usf.edu>
2022-01-28 14:25:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//programmer: william criqui
+
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("William Criqui has a ton of fun coding in Visual Studio");
+        }
+    }
+}
